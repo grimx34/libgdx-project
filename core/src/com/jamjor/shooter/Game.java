8a8e4beaace211a60e45392a40f14022c9ef150e
Jordan <jordanwatson94@yahoo.co.uk>
2015-01-24 17:45:43 +0000

    Reset

--- core/src/com/jamjor/shooter/Game.java
+++ core/src/com/jamjor/shooter/Game.java
@@ -1,27 +1,59 @@
 package com.jamjor.shooter;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
-public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+public class Game implements ApplicationListener, InputProcessor {
+
+
+	public class MyActor extends Actor {
+		Texture texture = new Texture(Gdx.files.internal("jet.png"));
+		@Override
+		public void draw(Batch batch, float alpha){
+			batch.draw((texture,Gdx.graphics.getWidth()/2)-,0);
+		}
+	}
+
+	private Stage stage;
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		stage = new Stage(new ExtendViewport(Gdx.graphics.getWidth(),Gdx.graphics.getHeight()));
+
+		MyActor myActor = new MyActor();
+		stage.addActor(myActor);
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void dispose() {
+		stage.dispose();
+	}
+
+	@Override
+	public void render() {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
 	}
 }
