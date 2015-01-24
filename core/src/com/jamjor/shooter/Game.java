899f344120540185ddfe6cea4f447e88e799638f
Jordan <jordanwatson94@yahoo.co.uk>
2015-01-24 01:22:27 +0000

    Suck my dick brah

--- core/src/com/jamjor/shooter/Game.java
+++ core/src/com/jamjor/shooter/Game.java
@@ -2,6 +2,7 @@ package com.jamjor.shooter;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -12,6 +13,7 @@ public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	BitmapFont font;
+	int x, y;
 	
 	@Override
 	public void create () {
@@ -31,8 +33,21 @@ public class Game extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+			x -= 5;
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+			x += 5;
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+			y += 5;
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+			y -= 5;
+		}
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y);
 		font.draw(batch, "Hello World!", 300, 300);
 		batch.end();
 	}
