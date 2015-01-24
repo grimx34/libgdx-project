0143134f3c1987e2d2cefe7bb83abf78b4648c2a
Jordan <jordanwatson94@yahoo.co.uk>
2015-01-24 16:54:42 +0000

    Reset

--- android/src/com/jamjor/shooter/android/AndroidLauncher.java
+++ android/src/com/jamjor/shooter/android/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.jamjor.shooter.game;
+import com.jamjor.shooter.Game;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new game(), config);
+		initialize(new Game(), config);
 	}
 }
--- desktop/src/com/jamjor/shooter/desktop/DesktopLauncher.java
+++ desktop/src/com/jamjor/shooter/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.jamjor.shooter.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.jamjor.shooter.game;
+import com.jamjor.shooter.Game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new game(), config);
+		new LwjglApplication(new Game(), config);
 	}
 }
